52cfc3d7dbfb1dcec85fcfdd87454668e3af0bc4
milo18818 <vippugster@gmail.com>
2023-02-24 18:39:21 +0000

    added second gamemode where the code ends if you get a question wrong and made difficulty 4 harder

--- main.py
+++ main.py
@@ -9,55 +9,112 @@ if os.path.isfile("highscore"):
     highscore = int(f.read())
     f.close()
 
-start_time = 0
-end_time = 0
-counter = 0
+print("select gamemode")
+gamemode = input()
+if gamemode == ("number"):
+    start_time = 0
+    end_time = 0
+    counter = 0
 
-score = 0
-number1 = 0
-number2 = 0
-print("select difficulty 1, 2, 3, 4")
-
-difficult =int(input())
-if difficult > 4:
-    print("invalid difficulty")
-    exit()
-
-if difficult == 1:
+    score = 0
     number1 = 0
-    number2 = 10
+    number2 = 0
+    print("select difficulty 1, 2, 3, 4")
 
-if difficult == 2:
+    difficult = int(input())
+    if difficult > 4:
+        print("invalid difficulty")
+        exit()
+
+    if difficult == 1:
+        number1 = 0
+        number2 = 10
+
+    if difficult == 2:
+        number1 = 0
+        number2 = 50
+
+    if difficult == 3:
+        number1 = 0
+        number2 = 100
+
+    if difficult == 4:
+        number1 = -9999999
+        number2 = 9999999
+
+    start_time = time.time()
+    for X in range(5):
+        random_number = random.randint(number1, number2)
+        random_number1 = random.randint(number1, number2)
+        print("what is " + str(random_number1) + "+" + str(random_number))
+        answer = input()
+        answer = answer.upper()
+        if answer == "X" or answer == "exit":
+            print("exited")
+            break
+        if int(answer) == (random_number1 + random_number):
+            score += 1
+        else:
+            print("wrong answer")
+    end_time = time.time() - start_time
+    end_time = int(end_time)
+    print("your score is " + str(score) + " you did it!" + " your time was " + str(end_time) + " seconds")
+    if highscore < score:
+        f = open("highscore", "w")
+        f.write(str(score))
+        f.close()
+        print("new high score")
+if gamemode == ("numberH"):
+    start_time = 0
+    end_time = 0
+    counter = 0
+
+    score = 0
     number1 = 0
-    number2 = 50
+    number2 = 0
+    print("select difficulty 1, 2, 3, 4")
 
-if difficult == 3:
-    number1 = 0
-    number2 = 100
+    difficult = int(input())
+    if difficult > 4:
+        print("invalid difficulty")
+        exit()
 
-if difficult == 4:
-    number1 = 0
-    number2 = 9999999
+    if difficult == 1:
+        number1 = 0
+        number2 = 10
 
-start_time = time.time()
-for X in range(5):
-    random_number = random.randint(number1, number2)
-    random_number1 = random.randint(number1, number2)
-    print("what is " + str(random_number1) + "+" + str(random_number))
-    answer = input()
-    answer = answer.upper()
-    if answer == "X" or answer == "exit":
-        print("exited")
-        break
-    if int(answer) == (random_number1 + random_number):
-        score += 1
-    else:
-        print("you are an idiot")
-end_time = time.time() - start_time
-end_time = int(end_time)
-print("your score is " + str(score) + " you did it!" + " your time was " + str(end_time) + " seconds")
-if highscore < score:
-    f = open("highscore", "w")
-    f.write(str(score))
-    f.close()
-    print("new high score")
+    if difficult == 2:
+        number1 = 0
+        number2 = 50
+
+    if difficult == 3:
+        number1 = 0
+        number2 = 100
+
+    if difficult == 4:
+        number1 = -9999999
+        number2 = 9999999
+
+    start_time = time.time()
+    for X in range(5):
+        random_number = random.randint(number1, number2)
+        random_number1 = random.randint(number1, number2)
+        print("what is " + str(random_number1) + "+" + str(random_number))
+        answer = input()
+        answer = answer.upper()
+        if answer == "X" or answer == "exit":
+            print("exited")
+            break
+        if int(answer) == (random_number1 + random_number):
+            score += 1
+        else:
+            print("wrong answer game over")
+            break
+    end_time = time.time() - start_time
+    end_time = int(end_time)
+    print("your score is " + str(score) + " you did it!" + " your time was " + str(end_time) + " seconds")
+    if highscore < score:
+        f = open("highscore", "w")
+        f.write(str(score))
+        f.close()
+        print("new high score")
